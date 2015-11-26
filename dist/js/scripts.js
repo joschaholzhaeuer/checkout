188352cd04d195ef7d1cc5d03decb055db606dce
Joscha Holzhaeuer <j.holzhaeuerapps@outlook.com>
2015-11-26 14:18:58 +0100

    put all into one file, steps animations done, rechnungs- und kreditbox toggles done

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4,39 +4,32 @@ JAVASCRIPT: MAIN.JS
 ******************************************************************/
 
 function adressbox() {
-    console.log('yes');
 
     var $label = $('.fields-label-check');
 
     if ($label.hasClass('checked')) {
         $label.removeClass('checked');
         $('.adressbox').removeClass('box--hidden');
+        $('.adressbox').addClass('box--transition');
     } else {
         $label.addClass('checked');
         $('.adressbox').addClass('box--hidden');
+        $('.adressbox').removeClass('box--transition');
     }
 }
 
-function kreditbox() {
+function showKreditbox() {
 
-    if ($('.kredit-toggle').hasClass('checked')) {
-        $('.kredit-toggle').removeClass('checked');
-        $('.kreditbox').hide();
-    } else {
-        $('.kredit-toggle').addClass('checked');
-        $('.kreditbox').show();
-    }
+    $('.kreditbox').removeClass('box--hidden');
+    $('.kreditbox').addClass('box--transition');
+
 }
 
-function scrollCart() {
-    var scroll_offset = $(window).scrollTop(),
-        cart_scroll_offset = cart_offset + scroll_offset;
+function hideKreditbox() {
 
-    $cart.css('top', cart_scroll_offset);
+    $('.kreditbox').addClass('box--hidden');
+    $('.kreditbox').removeClass('box--transition');
 
-    console.log(scroll_offset);
-    console.log(cart_offset);
-    console.log(cart_scroll_offset);
 }
 
 
@@ -45,6 +38,189 @@ $(document).ready(function($) {
     var $input = $('.fields-item-frame'),
         $cart  = $('.cart'),
         cart_offset = $cart.offset().top;
+
+
+    function scrollCart() {
+        var scroll_offset = $(window).scrollTop(),
+            cart_scroll_offset = cart_offset + scroll_offset;
+
+        $cart.css('top', cart_scroll_offset);
+
+        console.log(scroll_offset);
+        console.log(cart_offset);
+        console.log(cart_scroll_offset);
+    }
+
+
+    // Animate and switch tabs an nav clicks
+    $(document).on('click', '.steps-item', function() {
+
+        var $clicked_tab = $(this),
+        $tab_one     = $('.tab-one'),
+        $tab_two     = $('.tab-two'),
+        $tab_three   = $('.tab-three'),
+        $step_one    = $('.step-one'),
+        $step_two    = $('.step-two'),
+        $step_three  = $('.step-three');
+
+        // click on tab 1
+        if ($clicked_tab.hasClass('step-one') && !$clicked_tab.hasClass('steps-item--active')) {
+            $tab_one.removeClass('tab--inactive');
+            $tab_one.addClass('tab--active');
+            $tab_two.removeClass('tab--active');
+            $tab_two.addClass('tab--inactive');
+            $tab_three.removeClass('tab--active');
+            $tab_three.addClass('tab--inactive');
+
+            $step_one.addClass('steps-item--active');
+            if ($step_two.hasClass('steps-item--active')) {
+                $step_two.removeClass('steps-item--active');
+            } else if ($step_three.hasClass('steps-item--active')) {
+                $step_three.removeClass('steps-item--active');
+            }
+
+            if ($step_one.hasClass('steps-item--done')) {
+                $step_one.removeClass('steps-item--done');
+            }
+            if ($step_two.hasClass('steps-item--done')) {
+                $step_two.removeClass('steps-item--done');
+            }
+
+        // click on tab 2
+        } else if ($clicked_tab.hasClass('step-two') && !$clicked_tab.hasClass('steps-item--active')) {
+            $tab_two.removeClass('tab--inactive');
+            $tab_two.addClass('tab--active');
+            $tab_one.removeClass('tab--active');
+            $tab_one.addClass('tab--inactive');
+            $tab_three.removeClass('tab--active');
+            $tab_three.addClass('tab--inactive');
+
+            $step_two.addClass('steps-item--active');
+            $step_one.addClass('steps-item--done');
+            if ($step_one.hasClass('steps-item--active')) {
+                $step_one.removeClass('steps-item--active');
+            } else if ($step_three.hasClass('steps-item--active')) {
+                $step_three.removeClass('steps-item--active');
+            }
+
+            if ($step_two.hasClass('steps-item--done')) {
+                $step_two.removeClass('steps-item--done');
+            }
+
+        // click on tab 3
+        } else if ($clicked_tab.hasClass('step-three') && !$clicked_tab.hasClass('steps-item--active')) {
+            $tab_three.removeClass('tab--inactive');
+            $tab_three.addClass('tab--active');
+            $tab_one.removeClass('tab--active');
+            $tab_one.addClass('tab--inactive');
+            $tab_two.removeClass('tab--active');
+            $tab_two.addClass('tab--inactive');
+
+            $step_three.addClass('steps-item--active');
+            $step_one.addClass('steps-item--done');
+            $step_two.addClass('steps-item--done');
+            if ($step_one.hasClass('steps-item--active')) {
+                $step_one.removeClass('steps-item--active');
+            } else if ($step_two.hasClass('steps-item--active')) {
+                $step_two.removeClass('steps-item--active');
+            }
+        }
+
+        //smooth scroll to top
+        $('body,html').animate({
+            scrollTop: 0 ,
+            }, 300
+        );
+    });
+
+
+    // Animate and switch tabs on button clicks
+    $(document).on('click', '.button', function() {
+
+        var $clicked_button = $(this),
+        $tab_one     = $('.tab-one'),
+        $tab_two     = $('.tab-two'),
+        $tab_three   = $('.tab-three'),
+        $tab_four    = $('.tab-four'),
+        $step_one    = $('.step-one'),
+        $step_two    = $('.step-two'),
+        $step_three  = $('.step-three');
+
+        // click on button 1
+        if ($clicked_button.hasClass('button-one')) {
+            $tab_one.removeClass('tab--inactive');
+            $tab_one.addClass('tab--active');
+            $tab_two.removeClass('tab--active');
+            $tab_two.addClass('tab--inactive');
+            $tab_three.removeClass('tab--active');
+            $tab_three.addClass('tab--inactive');
+
+            $step_one.addClass('steps-item--active');
+            if ($step_two.hasClass('steps-item--active')) {
+                $step_two.removeClass('steps-item--active');
+            } else if ($step_three.hasClass('steps-item--active')) {
+                $step_three.removeClass('steps-item--active');
+            }
+
+            if ($step_one.hasClass('steps-item--done')) {
+                $step_one.removeClass('steps-item--done');
+            }
+
+        // click on button 2
+        } else if ($clicked_button.hasClass('button-two')) {
+            $tab_two.removeClass('tab--inactive');
+            $tab_two.addClass('tab--active');
+            $tab_one.removeClass('tab--active');
+            $tab_one.addClass('tab--inactive');
+            $tab_three.removeClass('tab--active');
+            $tab_three.addClass('tab--inactive');
+
+            $step_two.addClass('steps-item--active');
+            $step_one.addClass('steps-item--done');
+            if ($step_one.hasClass('steps-item--active')) {
+                $step_one.removeClass('steps-item--active');
+            } else if ($step_three.hasClass('steps-item--active')) {
+                $step_three.removeClass('steps-item--active');
+            }
+
+            if ($step_two.hasClass('steps-item--done')) {
+                $step_two.removeClass('steps-item--done');
+            }
+
+        // click on button 3
+        } else if ($clicked_button.hasClass('button-three')) {
+            $tab_three.removeClass('tab--inactive');
+            $tab_three.addClass('tab--active');
+            $tab_one.removeClass('tab--active');
+            $tab_one.addClass('tab--inactive');
+            $tab_two.removeClass('tab--active');
+            $tab_two.addClass('tab--inactive');
+
+            $step_three.addClass('steps-item--active');
+            $step_one.addClass('steps-item--done');
+            $step_two.addClass('steps-item--done');
+            if ($step_one.hasClass('steps-item--active')) {
+                $step_one.removeClass('steps-item--active');
+            } else if ($step_two.hasClass('steps-item--active')) {
+                $step_two.removeClass('steps-item--active');
+            }
+
+        // click on button 4
+        } else if ($clicked_button.hasClass('button-four')) {
+            $tab_four.removeClass('tab--inactive');
+            $tab_four.addClass('tab--active');
+            $tab_three.removeClass('tab--active');
+            $tab_three.addClass('tab--inactive');
+
+            $('.steps').hide();
+        }
+
+        //smooth scroll to top
+        $('body,html').animate({
+            scrollTop: 0 ,
+            }, 300
+        );
+    });
 
 
     // Show and hide placeholder and labels in input fields
@@ -61,7 +237,9 @@ $(document).ready(function($) {
 
     $input.on('focusout', function() {
         // Hide placeholder
-        $(this).siblings('.fields-item-placeholder--info').addClass('is-invisible');
+        if (!$(this).siblings('.fields-item-placeholder--info').hasClass('fields-item--prefilled')) {
+            $(this).siblings('.fields-item-placeholder--info').addClass('is-invisible');
+        }
 
         // Hide small label, if no input exists
         var value = $.trim($(this).val());
@@ -94,7 +272,7 @@ $(document).ready(function($) {
     });
 
 
-    // Show and hide additional boxes when checked/unchecked
+    // Show and hide rechnungsadresse box when checked/unchecked
     $(document).on('click', '.rechnung-toggle', function() {
 
         if (!$(this).data('isClicked')) {
@@ -110,10 +288,38 @@ $(document).ready(function($) {
         }
     });
 
-
+    // Show kreditkarten box when checked
     $(document).on('click', '.kredit-toggle', function() {
-        //kreditbox();
-        //console.log('test');
+
+        if (!$(this).data('isClicked')) {
+            var $trigger2 = $(this);
+
+            if (!$('#kreditkarte').is(':checked')) {
+                showKreditbox();
+            }
+
+            // Using a timer to prevent multiple clicks
+            $trigger2.data('isClicked', true);
+            setTimeout(function() {
+                $trigger2.removeData('isClicked');
+            }, 100);
+        }
+    });
+
+    // Hide kreditkarten box when unchecked
+    $(document).on('click', '.kredit-toggle--off', function() {
+
+        if (!$(this).data('isClicked')) {
+            var $trigger2 = $(this);
+
+            hideKreditbox();
+
+            // Using a timer to prevent multiple clicks
+            $trigger2.data('isClicked', true);
+            setTimeout(function() {
+                $trigger2.removeData('isClicked');
+            }, 100);
+        }
     });
 
 
